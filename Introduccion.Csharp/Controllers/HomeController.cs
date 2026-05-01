806fdf9460e1efbebb340c770fdaaff9036fbb8a
Karla <karlamartinezl2004@gmail.com>
2026-04-30 19:28:30 -0600

    Introduccion CSharp 30/04/2026

---
+++ Introduccion.Csharp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Introduccion.Csharp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Karla Lizbeth Martínez López";
+            ViewBag.Grupo = "IDGS904";
+            ViewBag.InitMessage = "Introduccion C#";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
